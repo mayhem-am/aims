7ce6556271ab62f425857aa0c57adbd44fd1129f
mayhem-am <amakanksh@gmail.com>
2020-11-07 16:07:11 +0530

    formed template to add user specific jobs

--- aims_/forms.py
+++ aims_/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from aims_.models import Broker, Admin,Company
 
@@ -12,8 +12,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    
-    role = StringField('Role',validators=[DataRequired()])
+    role = SelectField('Role',choices = ['Broker','Admin','Company'])
 
     submit = SubmitField('Sign Up')
 
@@ -50,21 +49,9 @@ class RegistrationForm(FlaskForm):
                 raise ValidationError('That email as Tester role is taken. Please choose a different one.')
         else:
             raise ValidationError('Invalid role entered.')
-    
-    def validate_role(self, role):
-        role = role.data.lower()
-        if role not in ["admin","broker","company"]:
-            raise ValidationError('Your role is undefined. Please choose one from Broker, Admin or Company.')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    role = StringField('Role',validators=[DataRequired()])
+    role = SelectField('Role',choices = ['Broker','Admin','Company'])
     submit = SubmitField('Login')
-
-    def validate_role(self, role):
-        role = role.data.lower()
-        if role not in ["admin","broker","company"]:
-            raise ValidationError('Your role is undefined. Please choose one from  Broker, Admin or Company.')
--- aims_/routes.py
+++ aims_/routes.py
@@ -3,7 +3,6 @@
 # from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort, session
 from aims_ import app, db, bcrypt
-# from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from aims_.forms import RegistrationForm, LoginForm
 from aims_.models import Broker, Admin, Company
 from flask_login import login_user, current_user, logout_user, login_required
@@ -15,7 +14,6 @@ from flask_login import login_user, current_user, logout_user, login_required
 def home():
     return render_template('home.html')
 
-#broker
 @app.route("/fellowsdisplay")
 @login_required
 def fellowdisp():
@@ -26,18 +24,6 @@ def fellowdisp():
     elif session['account_type'] == 'company':
         users = Company.query.all()
     return render_template('fellowsdisp.html',users = users)
-
-@app.route("/about")
-def about():
-    #-------------
-    print("Request ",end="")
-    print(request)
-    print(current_user) #User('ak', 'akanksh@example.com', 'default.jpg') or <flask_login.mixins.AnonymousUserMixin object at 0x0000019494BFA388> if not login
-    print(session)
-    #print(session['_user_id']) #cant access this here as this guy is anonymous as @login_required is not applied to this guy
-    #print(session['account_type'])
-    #------------
-    return render_template('about.html', title='About')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -74,9 +60,8 @@ def login():
                 user = Broker.query.filter_by(email=form.email.data).first()
                 if user: 
                     if bcrypt.check_password_hash(user.password, form.password.data):
-                        login_user(user, remember=form.remember.data)
+                        login_user(user)
                         next_page = request.args.get('next')
-                        
                         return redirect(next_page) if next_page else redirect(url_for('home'))
                     else:
                         flash(f'Welcome back {user.username}. Please check password', 'danger')
@@ -86,9 +71,8 @@ def login():
                 user = Admin.query.filter_by(email=form.email.data).first()
                 if user: 
                     if bcrypt.check_password_hash(user.password, form.password.data):
-                        login_user(user, remember=form.remember.data)
-                        next_page = request.args.get('next')
-                        
+                        login_user(user)
+                        next_page = request.args.get('next')                        
                         return redirect(next_page) if next_page else redirect(url_for('home'))
                     else:
                         flash(f'Welcome back {user.username}. Please check password', 'danger')
@@ -98,9 +82,8 @@ def login():
                 user = Company.query.filter_by(email=form.email.data).first()
                 if user: 
                     if bcrypt.check_password_hash(user.password, form.password.data):
-                        login_user(user, remember=form.remember.data)
+                        login_user(user)
                         next_page = request.args.get('next')
-                        
                         return redirect(next_page) if next_page else redirect(url_for('home'))
                     else:
                         flash(f'Welcome back {user.username}. Please check password', 'danger')
@@ -112,21 +95,9 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session['account_type'] # remove user from session to complement anonymous user mixin for 'current_user'
     logout_user()
     return redirect(url_for('home'))
-
-# @app.route("/account")
-# @login_required
-# def account():
-#     #-------------
-#     print("Request ",end="")
-#     print(request)
-#     print(current_user) #User('ak', 'akanksh@example.com', 'default.jpg') 
-#     print(session)   #<SecureCookieSession {'_fresh': True, '_id': 'fb94666e191c4de1198f13e41f735dfd8fbb27cff6226f6777bf3bbc1928c9d778c203afc99c194ea9e7501da9ec71446193196e7ea20e3f74b7a7919adea310', '_user_id': '1', 'csrf_token': '907b4853ddd27b84ae9cca324b5ad4d1bc679cdb'}> 
-#     print(session['_user_id'])
-#     #------------
-#     return render_template('account.html', title='Account')
-
 
 @app.route("/account/<int:user_id>/view")
 @login_required
@@ -137,11 +108,55 @@ def account(user_id):
         user = Admin.query.get_or_404(user_id)
     elif session['account_type'] == 'company':
         user = Company.query.get_or_404(user_id)
-    #-------------
-    print("Request ", end="")
-    print(request)
-    print(current_user)  # User('ak', 'akanksh@example.com', 'default.jpg')
-    print(session)  # <SecureCookieSession {'_fresh': True, '_id': 'fb94666e191c4de1198f13e41f735dfd8fbb27cff6226f6777bf3bbc1928c9d778c203afc99c194ea9e7501da9ec71446193196e7ea20e3f74b7a7919adea310', '_user_id': '1', 'csrf_token': '907b4853ddd27b84ae9cca324b5ad4d1bc679cdb'}>
-    print(session['_user_id'])
-    #------------
     return render_template('account.html', title='Account',userdetail = user)
+
+'''
+user specific routing
+'''
+@app.route("/processinvoices")
+@login_required
+def process_invoices():
+    if session['account_type']== 'broker':
+        return render_template('process_invoices.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
+
+@app.route("/assignbrokers")
+@login_required
+def assign_brokers():
+    if session['account_type']== 'admin':
+        return render_template('assign_brokers.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
+
+@app.route("/viewcompanies")
+@login_required
+def view_companies():
+    if session['account_type']== 'admin':
+        return render_template('view_companies.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
+
+@app.route("/uploadinvoice")
+@login_required
+def upload_invoice():
+    if session['account_type']== 'company':
+        return render_template('upload_invoice.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
+
+@app.route("/viewinvoices")
+@login_required
+def view_invoices():
+    if session['account_type']== 'company':
+        return render_template('view_invoices.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
+
+@app.route("/viewinventory")
+@login_required
+def view_inventory():
+    if session['account_type']== 'company':
+        return render_template('view_inventory.html', title='Process',userdetail = current_user)
+    else:
+        abort(403)
