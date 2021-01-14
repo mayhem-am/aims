6178953850edfa323faffc0e3864adb825a733c2
mayhem-am <amakanksh@gmail.com>
2021-01-14 19:48:09 +0530

    hide mongo-uri

--- aims_/__init__.py
+++ aims_/__init__.py
@@ -7,12 +7,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MONGO_URI'] = 'mongodb+srv://admin:1234@cluster0.zpmg5.mongodb.net/pres?retryWrites=true&w=majority' 
-"""
-mongodb+srv://admin:1234@cluster0.djdta.mongodb.net/ <dbname> ?retryWrites=true&w=majority      #akanksham.cs18@rvce.edu.in  <available database : mongodb>
-mongodb+srv://admin:1234@cluster0.zpmg5.mongodb.net/  <dbname>  ?retryWrites=true&w=majority    #amakanksh@gmail.com <available databases : test>
-<dbname> == database leading to collection u want to use
-"""
+app.config['MONGO_URI'] = ''
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
 mongodb = mongo.db
